05b74d03bc31b815a4df4cc73efec8836bbbcce5
Vignesh M <vigneshchary35@gmail.com>
2026-05-24 13:59:20 +0530

    inital

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,7 @@ import mv from "../Assets/mv.webp";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
+import pdf from "../Assets/Vignesh_M_Resume.pdf";
 import { ImBlog } from "react-icons/im";
 import {
   AiFillStar,
@@ -88,14 +89,13 @@ function NavBar() {
             </Nav.Item>
 
             <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/resume"
-                onClick={() => updateExpanded(false)}
-              >
-                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
-              </Nav.Link>
-            </Nav.Item>
+            <Nav.Link
+            href={pdf}
+            download="Vignesh_M_Resume.pdf"
+            onClick={() => updateExpanded(false)}>
+         <CgFileDocument style={{ marginBottom: "2px" }} />Download Resume
+        </Nav.Link>
+      </Nav.Item>
 
             <Nav.Item>
               <Nav.Link
